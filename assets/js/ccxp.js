f3a8d93f1ee5584f898faec9f62a6697f3371b69
lianvfigueiredo <lianvictor1@gmail.com>
2023-06-15 16:23:28 -0300

    Adding form and highlitghing card

--- assets/js/ccxp.js
+++ assets/js/ccxp.js
@@ -1,28 +1,76 @@
 const dataCCXP = new Date("20 jul 2023")
 
-    let quinta = document.getElementById('quinta')
-    quinta.addEventListener('mouseleave', destaqueOff)
-    quinta.addEventListener('mouseenter', destaque)
-    let sexta = document.getElementById('sexta')
-    sexta.addEventListener('mouseleave', destaqueOff)
-    sexta.addEventListener('mouseenter', destaque)
-    let sabado = document.getElementById('sabado')
-    sabado.addEventListener('mouseleave', destaqueOff)
-    sabado.addEventListener('mouseenter', destaque)
-    let domingo = document.getElementById('domingo')
-    domingo.addEventListener('mouseleave', destaqueOff)
-    domingo.addEventListener('mouseenter', destaque)
-    
+    document.addEventListener('keydown', selectedKey)
+    document.getElementById("comprar").addEventListener('click', comprar)
+    let ingressos = Array.from(document.querySelectorAll('.ingresso'))
+    let checks = Array.from(document.querySelectorAll('.check'))
+    let ingressosImg = Array.from(document.querySelectorAll('.ingressoimg'))
+
+    ingressos.forEach( (ingresso) => {
+        ingresso.addEventListener('mouseout', destaqueOff)
+        ingresso.addEventListener('mouseover', destaque)
+        ingresso.addEventListener('click', selectIngresso)}
+    )
     
     function destaque(){
-        this.classList.add("card-ingresso-selecionado")
-    }
+        this.classList.contains("card-ingresso-selecionado") ? null : this.classList.toggle("card-ingresso-selecionado")
+    }  
     function destaqueOff(){
-
-        this.style.transform = "scale(1)"
-        this.classList.remove("card-ingresso-selecionado")
-        this.style.transform = ""
+        this.classList.contains("card-ingresso-selecionado") ? this.classList.toggle("card-ingresso-selecionado") : null
+    }  
+    function remove(numero){
+        ingressos.forEach( (ingresso) => {
+            if(ingresso.classList.contains("card-ingresso-selecionado") && ingresso != ingressos[numero]){
+                ingresso.classList.toggle("card-ingresso-selecionado")
+            }
+        })
     }
+
+    setInterval(destacando, 3000)
+    
+    function destacando(){     
+        // k == ingressos.length ? k = 0 : null
+        for(let k = 0; k< ingressos.length; k++ ){
+            ingressos[k].classList.toggle("card-ingresso-selecionado")
+            setTimeout( () => {
+                ingressos[k].classList.toggle("card-ingresso-selecionado")
+                // k++
+            }, 2000)
+        }
+
+    }
+    
+    function selectedKey(event){
+        if(event.key == 'Enter'){
+            ingressos.forEach( (ingresso) => {
+                ingresso.classList.contains("card-ingresso-selecionado") ? selectIngresso.call(ingresso) : null
+
+            })
+        }
+        let numero = event.key - 1
+        if(numero >= 0 && numero <= 3){        
+            ingressos[numero].classList.toggle("card-ingresso-selecionado")
+            remove(numero)  
+        }
+    }
+    
+    function selectIngresso(){
+        let numero = ingressos.indexOf(this)
+        checks[numero].classList.toggle("display")
+    }
+
+    function comprar(){
+        let compra = document.getElementById("diasComprados")
+        compra.innerText = ""
+        checks.forEach( (element) => {
+            if(!element.classList.contains("display")){
+                compra.innerHTML += `${ingressosImg[checks.indexOf(element)].id} <br>`
+                // console.log(ingressos[0].valueOf("id"))
+            }
+        })
+        compra.innerText == "" ? document.getElementById("texto").classList.add("display") : document.getElementById("texto").classList.remove("display")
+    }
+
     function countdown(){
         const dataAtual = new Date()
         let diferenca = dataCCXP - dataAtual
